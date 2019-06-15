21da429f00e640079b0cb09d9bf3f6ca78f4edda
Juan Pablo Perez <Juan.Pablo.Perez@retailinmotion.com>
2019-06-15 21:11:35 +0100

    Create Vue Frontend with Vue CLI 3 0

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const { ApolloServer, gql } = require("apollo-server");
 const mongoose = require("mongoose");
-require("dotenv").config();
-
 const fs = require("fs");
 const path = require("path");
 
+// Import Environment variables from .env
+require("dotenv").config();
+
+// Import typedefs and resolvers
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
 const resolvers = require("./resolvers");
@@ -12,6 +14,7 @@ const resolvers = require("./resolvers");
 const User = require("./models/User");
 const Post = require("./models/Post");
 
+// Connect to MongoDb Atlas
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -20,6 +23,7 @@ mongoose
   .then(() => console.log("DB connected"))
   .catch(error => console.error(error));
 
+// Create Apollo GraphQL Server using typeDefs, resolvers, and context object
 const server = new ApolloServer({
   typeDefs,
   resolvers,
